6fd21e36824a856a69e4a742b2de60a10511f9fe
James McKinney <james@slashpoundbang.com>
2014-06-25 14:06:38 -0400

    update unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,24 @@
-worker_processes 2
+worker_processes 1
 timeout 30
-# https://newrelic.com/docs/troubleshooting/im-using-unicorn-and-i-dont-see-any-data
+# @see https://newrelic.com/docs/troubleshooting/im-using-unicorn-and-i-dont-see-any-data
 preload_app true
+
+# @see https://devcenter.heroku.com/articles/rails-unicorn
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
